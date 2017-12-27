c880b916973e1c429c14f95b86a803da03966fc8
Charles Stover <charlesstover@charlesstover.com>
2017-12-27 16:41:38 -0600

    v0.1.0

---
+++ async-action.js
@@ -0,0 +1,56 @@
+'use strict';
+
+require('whatwg-fetch');
+
+var asyncActionCreator = function(url) {
+	var body = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+	var onRequest = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+	var onReceive = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
+	var onError = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : null;
+	var cont = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : null;
+
+	return function (dispatch, getState) {
+
+		// If we have a condition for fetching, check if we should continue.
+		if (cont) {
+			if (!cont(getState())) {
+				return Promise.resolve();
+			}
+		}
+
+		// Action: Requesting data.
+		if (onRequest) {
+			dispatch(onRequest());
+		}
+
+		// Error Handler
+		var errorHandler = function errorHandler(error) {
+			if (onError) {
+				if (!error.message) {
+					error.message = 'Script error';
+				}
+				dispatch(onError(error));
+			}
+		};
+
+		// Fetch
+		return fetch(url, typeof body === 'function' ? body() : body).then(function (response) {
+			response.text().then(function (res2) {
+				if (response.status >= 400 && response.status < 600) {
+					try {
+						throw JSON.parse(res2);
+					} catch (e) {
+						throw new Error(res2);
+					}
+				}
+				if (onReceive) {
+					dispatch(onReceive(res2));
+				}
+			}).catch(errorHandler);
+		}).catch(errorHandler);
+	};
+};
+
+asyncActionCreator.default = asyncActionCreator;
+
+module.exports = asyncActionCreator;
---
+++ async-action.ts
@@ -0,0 +1,80 @@
+'use strict';
+
+type ActionCreator<T> = (param: T) => Object;
+type AsyncAction = (dispatch: Dispatch, getState: ObjectCreator) => Promise<void>;
+interface AsyncActionCreator {
+	default?: AsyncActionCreator;
+	(
+		url: string,
+		body: Object | ObjectCreator,
+		onRequest: null | ObjectCreator,
+		onReceive: ActionCreator<string> | null,
+		onError: ActionCreator<Error> | null,
+		cont: Continue | null
+	): AsyncAction;
+}
+type Continue = (state: Object) => boolean;
+type Dispatch = (action: Object) => any;
+type ObjectCreator = () => Object;
+
+import 'whatwg-fetch';
+
+export const asyncActionCreator: AsyncActionCreator = (url, body = {}, onRequest = null, onReceive = null, onError = null, cont = null) => {
+  return (dispatch, getState) => {
+
+    // If we have a condition for fetching, check if we should continue.
+    if (cont) {
+      if (!cont(getState())) {
+        return Promise.resolve();
+      }
+    }
+
+    // Action: Requesting data.
+    if (onRequest) {
+      dispatch(onRequest());
+    }
+
+    // Error Handler
+    const errorHandler = (error: Error) => {
+      if (onError) {
+				if (!error.message) {
+					error.message = 'Script error';
+				}
+        dispatch(onError(error));
+      }
+    }
+
+    // Fetch
+    return (
+			fetch(url, typeof body === 'function' ? body() : body)
+      .then(
+        (response: Response) => {
+					response
+					.text()
+					.then(
+						(res2: string) => {
+							if (
+								response.status >= 400 &&
+								response.status < 600
+							) {
+								try {
+									throw JSON.parse(res2);
+								}
+								catch (e) {
+									throw new Error(res2);
+								}
+							}
+							if (onReceive) {
+								dispatch(onReceive(res2));
+							}
+						}
+					)
+					.catch(errorHandler);
+        }
+			)
+			.catch(errorHandler)
+    );
+  };
+};
+
+asyncActionCreator.default = asyncActionCreator;
